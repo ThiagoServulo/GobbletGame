9ee43820f3156f403b5158b3ba56f763ff1f66c5
ThiagoServulo <thiago.servulo@sga.pucminas.br>
2022-07-19 16:10:59 -0300

    Padronização da classe ColorInfo finalizada

--- JogoDasTacasRussas/Entities/ColorInfo.cs
+++ JogoDasTacasRussas/Entities/ColorInfo.cs
@@ -1,6 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*******************************************************************************
+ * Arquivo: ColorInfo.cs                                                       *
+ * Autor: Thiago Sérvulo Guimarães                                             *
+ * Data: 17/07/2022                                                            *
+ *                                                                             *
+ * Classe: ColorInfo                                                           *
+ * Descrição: A classe 'ColorInfo' armazena as informações referentes as cores *
+ *   que uma determinada peça pode ter, bem como a sua cor atual.              *
+ * Atributos:                                                                  *
+ *   Primary [Color]: Cor primária da peça.                                    *
+ *   Secundary [Color]: Cor secundária da peça.                                *
+ *   TypeCurrent [ColorType]: Tipo atual da cor (primário ou secundário).      *
+ *******************************************************************************/
+
 using System.Drawing;
 using JogoDasTacasRussas.Entities.Enums;
 
@@ -8,25 +19,34 @@ namespace JogoDasTacasRussas.Entities
 {
     class ColorInfo
     {
-        public Color Primary;
-        public Color Secundary;
-        public ColorType TypeCurrent;
+        //----------------------------------------------------------------------
+        // Atributos
+        //----------------------------------------------------------------------
+        public Color Primary { get; private set; }
+        public Color Secundary { get; private set; }
+        public ColorType TypeCurrent { get; private set; }
 
+        //----------------------------------------------------------------------
+        // Construtor da classe 'ColorInfo'
+        //----------------------------------------------------------------------
         public ColorInfo(Color primary, Color secundary)
         {
             this.Primary = primary;
             this.Secundary = secundary;
-            this.TypeCurrent = ColorType.Primary;
+            this.TypeCurrent = ColorType.Primary; // A cor primária será a inicial
         }
 
+        //----------------------------------------------------------------------
+        // Descrição:
+        //    Função responsável por realizar a troca do tipo de cor atual da peça.
+        // Parâmetros:
+        //    Nenhum.
+        // Retorno:
+        //    Nenhum.
+        //----------------------------------------------------------------------
         public void ChangeColor()
         {
             this.TypeCurrent = (this.TypeCurrent == ColorType.Primary) ? ColorType.Secundary : ColorType.Primary;
-        }
-
-        public ColorType GetColorType()
-        {
-            return this.TypeCurrent;
         }
     }
 }
