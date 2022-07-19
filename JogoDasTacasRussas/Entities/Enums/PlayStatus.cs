5576c5a64212aed87edd96a2452464201b677dcf
ThiagoServulo <thiago.servulo@sga.pucminas.br>
2022-07-18 21:09:10 -0300

    Padronização da enumeração PlayStatus finalizada

--- JogoDasTacasRussas/Entities/Enums/PlayStatus.cs
+++ JogoDasTacasRussas/Entities/Enums/PlayStatus.cs
@@ -1,6 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*******************************************************************************
+ * Arquivo: PlayStatus.cs                                                      *
+ * Autor: Thiago Sérvulo Guimarães                                             *
+ * Data: 13/07/2022                                                            *
+ *                                                                             *
+ * Enumeração: PlayStatus                                                      *
+ * Descrição: A enumeração 'PlayStatus' agrupa os possíveis status que uma     *
+ *   jogada pode apresentar.                                                   *
+ * Tipo de dado dos itens: inteiro                                             *
+ * Itens da enumeração:                                                        *
+ *   WaitOriginField: Jogada ainda não iniciada, aguardando o campo de origem. *
+ *   WaitDestinyField: Campo de origem marcado, aguardando o campo de destino. *
+ *   Finish: Jogada finalizada, os campos de origem e destino foram marcados.  *
+ *******************************************************************************/
 
 namespace JogoDasTacasRussas.Entities.Enums
 {
